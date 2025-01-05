237f6d40b2f2223cfae8d56eb0c17943f06fd2e6
zohaib479 <zohaibrajput215@gmail.com>
2025-01-05 12:03:39 +0500

    Updated Utils

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 // require('dotenv').config()
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import app from "./app.js";
 dotenv.config(
    {
     path: "./env",
    } 
 )
-connectDB()
+connectDB().then(()=>{
+  app.listen(process.env.PORT||8000,()=>{
+    console.log(`server is running on port ${process.env.PORT}`)
+  })
+})
+.catch((error)=>{
+  console.log("CONNECTION FAILED",error)
+})
 
 
 
