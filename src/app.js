8176b17bfa976cf45b7502f25293f36adfdb7cdc
zohaib479 <zohaibrajput215@gmail.com>
2025-01-12 08:19:47 +0500

    Your commit message

--- src/app.js
+++ src/app.js
@@ -11,4 +11,9 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+import useRouter from "./routes/user.routes.js"
+
+app.use('/api/v1/users',useRouter)
+
 export default app
--- src/utils/Cloudnat.js
+++ src/utils/Cloudnat.js
@@ -5,3 +5,18 @@ import { v2 as cloudinary } from 'cloudinary';
         api_key:process.env.CLOUDINARY_API_KEY,
         api_secret:process.env.CLOUDINARY_API_SECRET
     });
+const uploadOnCloudinary = async (localFilePath)=>{
+    try{
+        if(!localFilePath) return null
+        const response =await cloudinary.uploader.upload(localFilePath,{
+            resource_type:"auto"
+        })
+        console.log("FILE UPLOADED ON CLOUDINARY ",response.url);
+        return response
+    }catch(error)
+    {
+        FileSystem.unlinkSync(localFilePath)//remove the locally saved temporary file as upload operation got failed 
+        return null;
+    }
+}
+export {uploadOnCloudinary}
